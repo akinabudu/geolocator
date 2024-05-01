3825d6d88ddb6e26baacfa701b578702215ee954
akin <orglobalng@gmail.com>
2024-05-01 13:28:45 +0100

    geo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,12 @@ import DeviceEventsPage from "./DeviceEvent/page";
 export default function Home() {
   return (
    <main className="flex min-h-screen flex-col items-center justify-around p-24">
-    <ModeToggle/>
-    <CardWithForm/>
+    {/* <ModeToggle/> */}
+    {/* <CardWithForm/> */}
+    <div className="flex flex-col items-center justify-center gap-4 border p-10 text-2xl">
+
     <DeviceEventsPage/>
+    </div>
    </main>
   );
 }
